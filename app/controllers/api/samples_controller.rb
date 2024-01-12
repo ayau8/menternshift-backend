14d495e99347c250cee25a061d5f362ac3eee0f5
ayau8 <ayaualvin@gmail.com>
2024-01-12 17:43:08 +0900

    Update instance variable to regular variable

--- app/controllers/api/samples_controller.rb
+++ app/controllers/api/samples_controller.rb
@@ -2,35 +2,35 @@ class Api::SamplesController < ApplicationController
   before_action :set_user, only: [:show, :update]
   
   def index
-    @users = User.all
-    render json: @users
+    users = User.all
+    render json: users
   end
 
   def show
-    render json: @user
+    render json: user
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if @user.update(user_params)
-      render json: @user
+    if user.update(user_params)
+      render json: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
   
   private
 
   def set_user
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
   end
 
   def user_params
