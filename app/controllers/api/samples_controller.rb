aa3870601f96ddb00e89ea83fdb3d40461a183dc
ayau8 <ayaualvin@gmail.com>
2024-01-12 17:34:57 +0900

    Fix indentation

--- app/controllers/api/samples_controller.rb
+++ app/controllers/api/samples_controller.rb
@@ -1,40 +1,39 @@
 class Api::SamplesController < ApplicationController
   before_action :set_user, only: [:show, :update]
-    
-    def index
-        @users = User.all
-        render json: @users
-    end
-    
-    def show
-        render json: @user
-    end
+  
+  def index
+    @users = User.all
+    render json: @users
+  end
 
-    def create
-        @user = User.new(user_params)
+  def show
+    render json: @user
+  end
 
-        if @user.save
-            render json: @user, status: :created
-        else
-            render json: @user.errors, status: :unprocessable_entity
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
 
-    def update
-        if @user.update(user_params)
-            render json: @user
-        else
-            render json: @user.errors, status: :unprocessable_entity
-        end
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
+  
+  private
 
-    private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def set_user
-        @user = User.find(params[:id])
-    end
-
-    def user_params
-        params.require(:user).permit(:username, :email)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
