daec7216ef16d0b3a2df7352649c9d949062c184
ayau8 <ayaualvin@gmail.com>
2024-01-19 13:21:38 +0900

    users keeps as regular var, user updates to instance var

--- app/controllers/api/v1/samples_controller.rb
+++ app/controllers/api/v1/samples_controller.rb
@@ -7,30 +7,30 @@ class Api::V1::SamplesController < ApplicationController
   end
 
   def show
-    render json: user
+    render json: @user
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: :created
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if user.update(user_params)
-      render json: user
+    if @user.update(user_params)
+      render json: @user
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
   
   private
 
   def set_user
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def user_params
