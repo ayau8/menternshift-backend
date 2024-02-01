d863cfb4c489e8a983a70139c7188c2ed31a477c
ayau8 <ayaualvin@gmail.com>
2024-02-01 16:52:02 +0900

    update show method and remove unneeded method call

--- app/controllers/Api/v1/users_controller.rb
+++ app/controllers/Api/v1/users_controller.rb
@@ -1,32 +1,10 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update]
-  
-  def index
-    users = User.all
-    render json: users
-  end
-  
+  before_action :set_user, only: [:show]
+    
   def show
-    render json: @user
+    render json: (@user ? @user : { error: "User not found" }), status: (@user ? :ok : :not_found)
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-  
   private
 
   def set_user
