349778699840673b8542ed417044ca3d3c260253
ayau8 <ayaualvin@gmail.com>
2024-02-01 14:35:24 +0900

    Include controllers for Users' API, update users' routes, create mentor's seed to test out User#show function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Cleaning the database
+puts "--------------------------------------"
+puts "Clearing DB"
+
+#Destroying the database
+User.destroy_all
+
+#Creating the users
+puts "--------------------------------------"
+puts "Creating the users"
+puts "--------------------------------------"
+puts "Creating mentors"
+puts "--------------------------------------"
+
+#mentors
+alvin = User.new(
+  email: "ayaualvin@gmail.com",
+  password: "123456",
+  role: "mentor",
+  first_name: "Alvin",
+  middle_name: "Ken",
+  last_name: "Yau",
+  username: "Alvin Yau",
+  location: "Tokyo",
+  company: "Menternshift",
+  job_title: "Software Engineer",
+  bio: "Hello, My name is Alvin Yau",
+  profile_img: "https://media.istockphoto.com/id/1142003972/photo/side-portrait-of-laughing-african-american-man-looking-up.jpg?s=612x612&w=0&k=20&c=fCiBfXI-PIdPACKbH3RdnYxq8ee5MZAhXKQN3I17j1Q=",
+  guidances: [
+    "Interview Coaching", 
+    "Skill Development", 
+    "Personal Development"
+  ],
+  languages: [
+    "English", 
+    "Japanese", 
+    "Cantonese", 
+    "Mandarin"
+  ],
+  skills: [],
+  is_available: "Y",
+  timezone: "Japan Standard Time (PST) UTC+09:00",
+  social_platforms: {
+    "LinkedIn" => "https://www.linkedin.com/in/alvin-yau/",
+    "Behance" => "",
+    "Instagram" => "",
+    "Portfolio" => "",
+  },
+  )
+alvin.save!  
