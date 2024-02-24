9e09257ef76134f74eec99ed69340699fa5b696d
ayau8 <ayaualvin@gmail.com>
2024-02-24 18:54:19 +0900

    add CREATE, UPDATE, DESTROY methods

--- app/controllers/Api/v1/expertises_controller.rb
+++ app/controllers/Api/v1/expertises_controller.rb
@@ -1,16 +1,46 @@
 class Api::V1::ExpertisesController < ApplicationController
-  before_action :set_expertise, only: [:show]
-    
+  before_action :set_expertise, only: [:show, :update, :destroy]
+  
   def show
     render json:
       @expertise ? @expertise : { error: "Expertise not found" },
       status: @expertise ? :ok : :not_found
   end
 
+  def new
+    @expertise = Expertise.new
+    render json: { expertise: @expertise }
+  end
+
+  def create
+    @user = User.find_by(id: params[:user_id])
+    @expertise = Expertise.new(expertise_params)
+    @expertise.user = User.find_by(id: @user.id)
+    if @expertise.save
+      render json: { expertise: @expertise }, status: :created
+    else
+      render json: { errors: @expertise.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @expertise.update(expertise_params)
+      render json: { expertise: @expertise }, status: :ok
+    else
+      render json: { errors: @expertise.errors }, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    render json:
+      @expertise ? @expertise.destroy : { error: "Expertise not found" },
+      status: @expertise ? :no_content : :not_found
+  end
+
   private
 
   def set_expertise
-    @expertise = Expertise.where(user_id: params[:user_id]).find_by(id: params[:id])
+    @expertise = Expertise.find_by(user_id: params[:user_id], id: params[:id])
   end
 
   def expertise_params
--- app/models/expertise.rb
+++ app/models/expertise.rb
@@ -1,3 +1,7 @@
 class Expertise < ApplicationRecord
   belongs_to :user
+
+  validates :domain, presence: true
+  validates :years_of_experience, presence: true, length: { minimum: 1 }
+
 end
