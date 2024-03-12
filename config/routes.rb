785d3ed14f147e985cd15e007a5e6dab0688c84b
Sarah Rollins <s.rollins751@gmail.com>
2024-03-12 16:15:40 +0900

    installed rswag gem and associated files

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@
 # root "articles#index"
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
 
   namespace :api do
