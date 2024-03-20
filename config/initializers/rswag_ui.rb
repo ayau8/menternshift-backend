55e4e1bf5268666819fe50689b060faaef7b7e88
Sarah Rollins <s.rollins751@gmail.com>
2024-03-20 13:22:35 +0900

    UI- expanded model rows so data type is visible

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -9,6 +9,7 @@ Rswag::Ui.configure do |c|
   # correspond to the relative paths for those endpoints.
 
   c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
+  c.config_object['defaultModelsExpandDepth'] = 4
 
   # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
